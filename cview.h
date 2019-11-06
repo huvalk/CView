3da69cf5eb9f82665c803c9cf590d86f9a615aa8
huvalk <akiraq@yandex.ru>
2019-11-06 18:56:34 +0300

    CView prototype

--- cview.h
+++ cview.h
@@ -2,16 +2,60 @@
 #define CVIEW_H
 
 #include <QObject>
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
-class CView : public QObject
+struct Meta;
+struct Graph;
+
+struct SRoomMeta; // структура, соответсвтующая комнате
+struct SConnectionMeta; // структура, соответствующая соединению
+
+class CGraphicConnection : public QGraphicsLineItem
+{
+
+};
+
+class CGraphicRoom : public QGraphicsEllipseItem
+{
+
+};
+
+class CGraphicsScene : public QGraphicsScene
+{
+
+};
+
+class CGraphicsView : public QGraphicsView
 {
     Q_OBJECT
+
 public:
-    explicit CView(QObject *parent = nullptr);
+    explicit CGraphicsView( QObject *parent = nullptr );
+
+private:
+    QVector<CGraphicRoom> rooms_;
+    QVector<CGraphicConnection> connections_;
 
 signals:
+    void SaveNewRoom( SRoomMeta & new_room );
+    void SaveNewConnection( SConnectionMeta & new_connection );
+
+    void ChooseRoom( int room_id );
+    void ChooseConnection( int connection_id );
 
 public slots:
+    void BuildItems( const Meta & new_meta, const Graph & new_graph );
+    void RefreshItems( const Meta & new_meta, const Graph & new_graph,
+                       QVector< int > rooms_change_list);
+
+    void DrawThePath( QVector< int > path );
+
+    void CreateNewConnectin();
+    void CreateNewRoomMeta();
+    void ChangeRoomMeta();
+
 };
 
 #endif // CVIEW_H
